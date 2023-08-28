1de2d84127fbcefc1455a263a5169fb503fc7585
Austin Hammer <austin@getsavi.com>
2023-08-28 12:47:11 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SchemaGenerator.MixProject do
   def project do
     [
       app: :schema_generator,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       description: description(),
       start_permanent: Mix.env() == :prod,
